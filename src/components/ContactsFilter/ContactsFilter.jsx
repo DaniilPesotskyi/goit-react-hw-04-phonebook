24884208b6b17235566a569e429451a020a10af8
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2023-05-23 10:41:16 +0300

    ghp fix & prt

--- src/components/ContactsFilter/ContactsFilter.jsx
+++ src/components/ContactsFilter/ContactsFilter.jsx
@@ -1,28 +1,29 @@
-import React, { Component } from "react";
-import css from './ContactsFilter.module.css'
+import React, { Component } from 'react';
+import css from './ContactsFilter.module.css';
 
 class ContactsFilter extends Component {
+  onChangeQuery = e => {
+    this.props.onFilter(e.currentTarget.value);
+  };
 
-    onChangeQuery = e => {
-        this.props.onFilter(e.currentTarget.value)
-    }
-
-    render() {
-        return (
-            <div className={css.inputField}>
-                <input 
-                    autoComplete="off"
-                    className={css.input}
-                    type="text" 
-                    placeholder="."
-                    name="query" 
-                    id="query" 
-                    onChange={e => this.onChangeQuery(e)}
-                />
-                <label className={css.label} htmlFor="query">Search</label>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={css.inputField}>
+        <input
+          autoComplete="off"
+          className={css.input}
+          type="text"
+          placeholder="."
+          name="query"
+          id="query"
+          onChange={e => this.onChangeQuery(e)}
+        />
+        <label className={css.label} htmlFor="query">
+          Search
+        </label>
+      </div>
+    );
+  }
 }
 
-export default ContactsFilter
+export default ContactsFilter;
--- src/components/ContactsItem/ContactsItem.jsx
+++ src/components/ContactsItem/ContactsItem.jsx
@@ -1,13 +1,15 @@
-import css from './ContactsItem.module.css'
+import css from './ContactsItem.module.css';
 
-const ContactsItem = ({id, number, name, onDelete}) => {
-    return (
-        <li className={css.item}>
-            <p className={css.name}>{name}</p>
-            <p className={css.number}>{number}</p>
-            <button className={css.button} type="button" onClick={() => onDelete(id)}>Delete</button>
-        </li>
-    )
-}
+const ContactsItem = ({ id, number, name, onDelete }) => {
+  return (
+    <li className={css.item}>
+      <p className={css.name}>{name}</p>
+      <p className={css.number}>{number}</p>
+      <button className={css.button} type="button" onClick={() => onDelete(id)}>
+        Delete
+      </button>
+    </li>
+  );
+};
 
-export default ContactsItem
+export default ContactsItem;
